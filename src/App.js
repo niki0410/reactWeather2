a9a8f5f15ba09f2bfcc578752c9fbf2eb71909d4
niki0410 <nikiboni13@gmail.com>
2025-02-06 12:38:28 +0100

    css, url

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ export default function App() {
         <footer>
         <span>This project was coded by </span> 
         <a href="https://github.com/niki0410" target="_blank" rel="noreferrer">Nikolett Hartmann</a> <span> and is </span>
-        <a href="https://github.com/niki0410/react-weather" target="_blank" rel="noreferrer">on GitHub</a><span> and </span>  
-        <a href="https://app-react-weather-app.netlify.app/" target="_blank" rel="noreferrer">hosted on Netlify</a>
+        <a href="https://github.com/niki0410/reactWeather2" target="_blank" rel="noreferrer">on GitHub</a><span> and </span>  
+        <a href="https://weather-react-app-2.netlify.app/" target="_blank" rel="noreferrer">hosted on Netlify</a>
   
         </footer>
       </div>
